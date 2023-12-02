5e18d15b9656b72ca0f08c49281e62e51d254c79
SesiSenai1DE <sesisenai.1de@gmail.com>
2023-12-02 10:22:19 -0300

    Inclusão
    Inclusão BD para cadastro de clientes.

---
+++ wezoo (V2)/create.php
@@ -0,0 +1,34 @@
+<?php
+  include_once("templates/header.php");
+?>
+  <div class="container">
+    <?php include_once("templates/backbtn.html"); ?>
+    <h1 id="main-title">Criar contato</h1>
+    <form id="create-form" action="<?= $BASE_URL ?>config/process.php" method="POST">
+      <input type="hidden" name="type" value="create">
+      <div class="form-group">
+        <label for="name">Nome do contato:</label>
+        <input type="text" class="form-control" id="name" name="name" placeholder="Digite o nome" required>
+      </div>
+      <div class="form-group">
+        <label for="phone">Telefone do contato:</label>
+        <input type="text" class="form-control" id="phone" name="phone" placeholder="Digite o telefone" required>
+      </div>
+      <div class="form-group">
+        <label for="email">E-mail:</label>
+        <input type="text" class="form-control" id="email" name="email" placeholder="Digite o e-mail" required>
+      </div>
+      <div class="form-group">
+        <label for="text">Senha:</label>
+        <input type="text" class="form-control" id="senha" name="senha" placeholder="Digite a sua senha" required>
+      </div>
+      <div class="form-group">
+        <label for="observations">Observações:</label>
+        <textarea type="text" class="form-control" id="observations" name="observations" placeholder="Insira as observações" rows="3"></textarea>
+      </div>
+      <button type="submit" class="btn btn-success">Cadastrar</button>
+    </form>
+  </div>
+<?php
+  include_once("templates/footer.php");
+?>
